555f5d0561c3477b04d0cdf243cb01c72a9741ab
Paul W. Frields <stickster@gmail.com>
2024-07-21 13:48:51 -0400

    Added new EventProcessor: Random Drop

---
+++ lua/Random Drop.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------
+--! Random Drop
+--! Randomly drop events
+--! Copyright (C) 2024 Paul W. Frields / Fifth Dominion Studios
+--! License: BSD - https://opensource.org/license/bsd-3-clause/
+-----------------------------------------------------------------
+
+Label{"Drop events random based on probability setting", align = "centred", width = 710}
+
+skipKnob = Knob{"Skip", 0, 0, 1, false}
+
+function onNote(e)
+    local myRandom = math.random()
+    if (myRandom <= skipKnob.value) then
+        return
+    else
+        playNote(e.note, e.velocity, e.duration, e.layer, e.channel, e.input, e.vol, e.pan, e.tune, e.slice)
+    end
+end
